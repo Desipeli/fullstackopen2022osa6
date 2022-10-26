fcdae542dd9b7a579f2e1a8a0fee4ec32c1b3355
Mika Desideri <mr.desipeli@gmail.com>
2022-10-26 15:54:38 +0300

    6.13, 6.14

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,21 @@
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { getAll } from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 
 const App = () => {
 
+  const dispatch = useDispatch()
+  useEffect(() => {
+    console.log()
+    getAll().then(anecdotes =>
+      dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
 
   return (
     <div>
--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,18 @@
 import { useDispatch } from "react-redux"
-import { createAnecdote } from "../reducers/anecdoteReducer"
+import { appendAnecdote, createAnecdote } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
+import { saveAnecdote } from "../services/anecdotes"
 
 const AnecdoteForm = () => {
 
     const dispatch = useDispatch()
 
-    const create = (event) => {
+    const create = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await saveAnecdote(content)
+        dispatch(appendAnecdote(newAnecdote))
         dispatch(setNotification(
           `Created anecdote: ${content}`
         ))
