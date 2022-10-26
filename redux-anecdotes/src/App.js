4e536935cb568d21391e082b65cb5c2b5182eb44
Mika Desideri <mr.desipeli@gmail.com>
2022-10-26 16:40:39 +0300

    6.15

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -4,17 +4,14 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import { getAll } from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 
 const App = () => {
-
   const dispatch = useDispatch()
+
   useEffect(() => {
-    console.log()
-    getAll().then(anecdotes =>
-      dispatch(setAnecdotes(anecdotes)))
+      dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
-
-
-// const anecdotesAtStart = [
-//   'If it hurts, do it more often',
-//   'Adding manpower to a late software project makes it later!',
-//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//   'Premature optimization is the root of all evil.',
-//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-// ]
+import { getAll } from "../services/anecdotes"
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -20,7 +11,6 @@ const asObject = (anecdote) => {
   }
 }
 
-// const initialState = anecdotesAtStart.map(asObject)
 const initialState = []
 
 const anecdoteSlice = createSlice({
@@ -51,6 +41,14 @@ const anecdoteSlice = createSlice({
     }
   }
 })
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await getAll()
+    console.log(anecdotes)
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export const { createAnecdote, voteAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
